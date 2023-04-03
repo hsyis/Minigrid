43a2259e2921856b77fa9d5b044a9256c2c439ec
seungyeon-hwang <hsyis@kaist.ac.kr>
2023-04-03 21:24:44 +0900

    setup for new env

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup
 
 setup(
     name='gym_minigrid',
-    version='1.0.2',
+    version='1.0.3',
     keywords='memory, environment, agent, rl, openaigym, openai-gym, gym',
     url='https://github.com/maximecb/gym-minigrid',
     description='Minimalistic gridworld package for OpenAI Gym',
     packages=['gym_minigrid', 'gym_minigrid.envs'],
     install_requires=[
-        'gym>=0.9.6',
+        'setuptools==65.7.0',
+        'gym==0.21.0',
+        'matplotlib==3.7.1',
         'numpy>=1.15.0'
     ]
 )
